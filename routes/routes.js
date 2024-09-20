be677c7c73b7eed0a958f3c8a5d73e9ac2d00969
tony figueras <tonfy01@gmail.com>
2024-09-20 12:37:56 -0400

    correccion declaracion ruta

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,11 @@
-import express from "express";  //const express = require('express');
+
+const express = require("express")
+const router = express.Router()
+//import express from "express";  //const express = require('express');
 import { login, register, logout } from "../controller/authController.js"; //const { login, register, logout } = require('../controllers/authController');
 import createToken from('../controllers/hederaController.js');
 import authMiddleware from('../middleware/authMiddleware.js');
-const router = express.Router() //const router = express.Router();
+//const router = express.Router() //const router = express.Router();
 
 // Rutas de autenticación
 router.post('/login', login);
@@ -14,7 +17,7 @@ router.post('/create-token-hedera', authMiddleware, createToken);
 router.get('/list-tokens', authMiddleware, listTokens); // Nuevo endpoint para listar tokens
 
 
-export default router  //module.exports = router;
+module.exports = router;
 
 
 
