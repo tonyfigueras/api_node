7ab89fe8f11bd157da623bbeabdbf2cb10a28ce0
tony figueras <tonfy@example.com>
2024-09-20 19:32:17 -0400

    vercel

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ const express = require("express")
 const router = express.Router()
 const authController = require('../controllers/authController');
 const hederaController = require('../controllers/hederaController');
-const authMiddleware = require('../middleware/authMiddleware');
+//const authMiddleware = require('../middleware/authMiddleware');
 router.post('/login', authController.login);
 router.post('/register', authController.register);
 //router.post('/logout', authMiddleware, authController.logout);
